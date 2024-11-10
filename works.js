9c474ac0755a487608b0987059ab65d192290a23
Ryan Chia <ryan_chia8@hotmail.com>
2024-11-10 12:34:27 +0800

    asd

--- works.js
+++ works.js
@@ -2,7 +2,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const Chrome = require("selenium-webdriver/chrome");
 
 (async function test() {
-    const options = new Chrome.Options().headless();
+    const options = new Chrome.Options().addArguments("--headless");
     const builder = new Builder().forBrowser('chrome').setChromeOptions(options);
 
     console.log(builder.getChromeOptions());
