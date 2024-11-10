261f7d39c586c99b593c1a2f455f9725f86aaa38
Ryan Chia <ryan_chia8@hotmail.com>
2024-11-10 12:07:42 +0800

    asd

---
+++ works.js
@@ -0,0 +1,18 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const Chrome = require("selenium-webdriver/chrome");
+
+(async function test() {
+    const builder = new Builder().forBrowser('chrome');
+
+    console.log(builder.getChromeOptions());
+    
+    let driver = await builder.build();
+    
+    try {
+        await driver.get('http://www.google.com');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
